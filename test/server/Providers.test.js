bbed041947b7cf07fc24f5590b3a14e49deddb53
Wojciech Gawronski <afronski@gmail.com>
2014-01-01 15:42:22 +0100

    [#8] Preparing to the 'TASK 2' - created asynchronous API and LevelDB dependencies added.

--- test/server/Providers.test.js
+++ test/server/Providers.test.js
@@ -11,41 +11,135 @@ describe("Rooms provider", function () {
         this.room = { name: "Room 1", players: [] };
     });
 
-    afterEach(Rooms.clear);
-
-    it("should have ability to add new room to the storage", function () {
-        Rooms.get().length.should.be.equal(0);
-
-        Rooms.add(this.room);
-        Rooms.get().length.should.be.equal(1);
+    afterEach(function (finish) {
+        Rooms.clear(finish);
     });
 
-    it("should have ability to clear storage", function () {
-        Rooms.add(this.room);
-        Rooms.get().length.should.not.be.equal(0);
+    it("should have ability to add new room to the storage", function (finish) {
+        Rooms.get(function (error, results) {
+            if (error) {
+                finish(error);
+                return;
+            }
 
-        Rooms.clear();
-        Rooms.get().length.should.be.equal(0);
+            results.length.should.be.equal(0);
+
+            Rooms.add(this.room, function (error) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
+
+                Rooms.get(function (error, results) {
+                    if (error) {
+                        finish(error);
+                        return;
+                    }
+
+                    results.length.should.be.equal(1);
+
+                    finish();
+                });
+            });
+        });
     });
 
-    it("should have ability to remove room from the storage", function () {
-        Rooms.remove("Room 1");
-        Rooms.get().length.should.be.equal(0);
+    it("should have ability to clear storage", function (finish) {
+        Rooms.add(this.room, function (error) {
+            if (error) {
+                finish(error);
+                return;
+            }
 
-        Rooms.add(this.room);
-        Rooms.remove("Room 1");
-        Rooms.get().length.should.be.equal(0);
+            Rooms.get(function (error, results) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
+
+                results.length.should.not.be.equal(0);
+
+                Rooms.clear(function (error) {
+                    if (error) {
+                        finish(error);
+                        return;
+                    }
+
+                    Rooms.get(function (error, results) {
+                        if (error) {
+                            finish(error);
+                            return;
+                        }
+
+                        results.length.should.be.equal(0);
+
+                        finish();
+                    });
+                });
+            });
+        });
     });
 
-    it("should have ability to get all rooms from the storage", function () {
-        var results;
+    it("should have ability to remove room from the storage", function (finish) {
+        Rooms.remove("Room 1", function (error) {
+            if (error) {
+                finish(error);
+                return;
+            }
 
-        Rooms.add(this.room);
-        results = Rooms.get();
+            Rooms.get(function (error, results) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
 
-        results.length.should.be.equal(0);
-        results[0].name.should.be.equal("Room 1");
-        results[0].players.length.should.be.equal(0);
+                results.length.should.be.equal(0);
+
+                Rooms.add(this.room, function (error) {
+                    if (error) {
+                        finish(error);
+                        return;
+                    }
+
+                    Rooms.remove("Room 1", function (error) {
+                        if (error) {
+                            finish(error);
+                            return;
+                        }
+
+                        Rooms.get(function (error, results) {
+                            if (error) {
+                                finish(error);
+                                return;
+                            }
+
+                            results.length.should.be.equal(0);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it("should have ability to get all rooms from the storage", function (finish) {
+        Rooms.add(this.room, function (error) {
+            if (error) {
+                finish(error);
+                return;
+            }
+
+            Rooms.get(function (error, results) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
+
+                results.length.should.be.equal(0);
+
+                results[0].name.should.be.equal("Room 1");
+                results[0].players.length.should.be.equal(0);
+            });
+        });
     });
 
 });
@@ -56,41 +150,135 @@ describe.skip("ScoresProvider", function () {
         this.score = { roomName: "Room 1", players: [] };
     });
 
-    afterEach(Scores.clear);
-
-    it("should have ability to add new score list to the storage", function () {
-        Scores.get().length.should.be.equal(0);
-
-        Scores.add(this.score);
-        Scores.get().length.should.be.equal(1);
+    afterEach(function (finish) {
+        Scores.clear(finish);
     });
 
-    it("should have ability to clear storage", function () {
-        Scores.add(this.score);
-        Scores.get().length.should.not.be.equal(0);
+    it("should have ability to add new score to the storage", function (finish) {
+        Scores.get(function (error, results) {
+            if (error) {
+                finish(error);
+                return;
+            }
 
-        Scores.clear();
-        Scores.get().length.should.be.equal(0);
+            results.length.should.be.equal(0);
+
+            Scores.add(this.score, function (error) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
+
+                Scores.get(function (error, results) {
+                    if (error) {
+                        finish(error);
+                        return;
+                    }
+
+                    results.length.should.be.equal(1);
+
+                    finish();
+                });
+            });
+        });
     });
 
-    it("should have ability to remove score list from the storage", function () {
-        Scores.remove("Room 1");
-        Scores.get().length.should.be.equal(0);
+    it("should have ability to clear storage", function (finish) {
+        Scores.add(this.score, function (error) {
+            if (error) {
+                finish(error);
+                return;
+            }
 
-        Scores.add(this.room);
-        Scores.remove("Room 1");
-        Scores.get().length.should.be.equal(0);
+            Scores.get(function (error, results) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
+
+                results.length.should.not.be.equal(0);
+
+                Scores.clear(function (error) {
+                    if (error) {
+                        finish(error);
+                        return;
+                    }
+
+                    Scores.get(function (error, results) {
+                        if (error) {
+                            finish(error);
+                            return;
+                        }
+
+                        results.length.should.be.equal(0);
+
+                        finish();
+                    });
+                });
+            });
+        });
     });
 
-    it("should have ability to get all score lists from the storage", function () {
-        var results;
+    it("should have ability to remove score from the storage", function (finish) {
+        Scores.remove("Room 1", function (error) {
+            if (error) {
+                finish(error);
+                return;
+            }
 
-        Scores.add(this.room);
-        results = Scores.get();
+            Scores.get(function (error, results) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
 
-        results.length.should.be.equal(0);
-        results[0].roomName.should.be.equal("Room 1");
-        results[0].players.length.should.be.equal(0);
+                results.length.should.be.equal(0);
+
+                Scores.add(this.score, function (error) {
+                    if (error) {
+                        finish(error);
+                        return;
+                    }
+
+                    Scores.remove("Room 1", function (error) {
+                        if (error) {
+                            finish(error);
+                            return;
+                        }
+
+                        Scores.get(function (error, results) {
+                            if (error) {
+                                finish(error);
+                                return;
+                            }
+
+                            results.length.should.be.equal(0);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it("should have ability to get all score lists from the storage", function (finish) {
+        Scores.add(this.score, function (error) {
+            if (error) {
+                finish(error);
+                return;
+            }
+
+            Scores.get(function (error, results) {
+                if (error) {
+                    finish(error);
+                    return;
+                }
+
+                results.length.should.be.equal(0);
+
+                results[0].roomName.should.be.equal("Room 1");
+                results[0].players.length.should.be.equal(0);
+            });
+        });
     });
 
 });
